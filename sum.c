1d35686bad9bf2d658d9d75d48aab30da86e3c4a
Tom Victor <vjtomvictor@icloud.com>
2017-01-04 23:06:05 +0530

    Sum code

--- sum.c
+++ sum.c
@@ -2,9 +2,12 @@
 
 int main(int argc, char const *argv[])
 {
-	int a=0,b=0,s=0 ;
-
-	printf("%f\n",&a );
-
+	int a = 0, b = 0, s = 0 ;
+	printf("\nenter the first value _ \n");
+	scanf("%d",a);
+	printf("enter the second value _ \n");
+	scanf("%d",b)
+	s = a + b ;
+	printf("\nsum = %d", s);
 	return 0;
 }
