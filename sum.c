dcbc8842b9e6f07a78de5b02fd3e4fec8489e5b1
Tom Victor <vjtomvictor@icloud.com>
2017-01-05 07:39:32 +0530

    Update

--- sum.c
+++ sum.c
@@ -2,11 +2,11 @@
 
 int main(int argc, char const *argv[])
 {
-	int a = 0, b = 0, s = 0 ;
+	int a , b , s = 0 ;
 	printf("\nenter the first value _ \n");
 	scanf("%d",a);
 	printf("enter the second value _ \n");
-	scanf("%d",b)
+	scanf("%d",b);
 	s = a + b ;
 	printf("\nsum = %d", s);
 	return 0;
