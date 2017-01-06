32885940207ead7f0a49964fae9a83bc56dce7f9
Tom Victor <vjtomvictor@icloud.com>
2017-01-07 00:26:05 +0530

    Simple expts

--- fibno.c
+++ fibno.c
@@ -3,16 +3,17 @@
 int main(int argc, char const *argv[])
 {
 	int a = 0, b = 1, c = 0,temp = 0;
-	const char n ;
-	printf("Enter the range : \n", );
-	scanf("%d",n);
-	while(n){
-		printf("%d\n",a );
-		temp = a + b ;
-		a = temp ;
-		b++ ;
+	char n[10] ;
+	printf("Enter the range : \n" );
+	scanf("%s",n);
+	printf("%s\n",n );
+	// while(b){
+	// 	printf("%d\n",a );
+	// 	temp = a + b ;
+	// 	a = temp ;
+	// 	b++ ;
 
-	}
+	// }
 
 	return 0;
 }
