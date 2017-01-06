cf0f8ca11a7054e1b9f007b4eb15c339160e5ad5
Tom Victor <vjtomvictor@icloud.com>
2017-01-07 00:27:55 +0530

    Updates

--- fibno.c
+++ fibno.c
@@ -2,11 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
-	int a = 0, b = 1, c = 0,temp = 0;
-	char n[10] ;
+	int a = 0, b = 1, c = 0,temp = 0, n = 0;
 	printf("Enter the range : \n" );
-	scanf("%s",n);
-	printf("%s\n",n );
+	scanf("%d",n);
+	printf("%d\n",n );
 	// while(b){
 	// 	printf("%d\n",a );
 	// 	temp = a + b ;
