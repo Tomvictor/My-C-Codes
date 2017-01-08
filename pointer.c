2410d1a28cf34df06600dfb765124596545554cd
Tom Victor <vjtomvictor@icloud.com>
2017-01-08 23:37:27 +0530

    Code not working..
    To be fixed

--- pointer.c
+++ pointer.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
+void swap(int,int);
+
 int main(int argc, char const *argv[])
 {
-	int x = 10;
-	printf("%d\n", &x);
-	return 0;
+	int a, b ;
+	a = 10 ;
+	b = 20 ;
+	printf("a before swap is %d\n",a );
+	printf("b before wasp is %d\n",b );
+	swap(&a, &b);
+	printf("a after swap is %d\n",a );
+	printf("b after swap is %d\n",b );
+
+	return 0 ;
+}
+
+void swap(int *x, int *y)
+{
+	int temp = *x ;
+	*x = 1 ;
+	*y = temp ;
+	return ;
 }
