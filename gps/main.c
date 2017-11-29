abc8e2dd5b8062fa356ce7f66d4d99330b55aea1
Tom Victor <vjtomvictor@icloud.com>
2017-11-29 19:51:49 +0530

    working source

--- gps/main.c
+++ gps/main.c
@@ -3,35 +3,46 @@
 
 char gp_buff[] = "$GPRMC,103956.000,A,1009.9733,N,07618.6494,E,0.00,272.93,120517,,,D*6F" ;
 
-int temp = 3 ;
+
+
 
 int main(void)
 {
-  printf("\nnmea parsing code started... \n");
   printf("\nbuff :  %s\n", gp_buff);
-  get_lat_str(gp_buff,temp);
+  get_lat(gp_buff);
+  get_lat_hem(gp_buff) ;
+  get_lon(gp_buff) ;
+  get_lon_hem(gp_buff) ;
+
   return 0;
 }
 
-get_lat_str(*buff,int count){
+get_lat(*buff){
+  parse_nmea(buff,3);
+}
+
+get_lat_hem(*buff){
+  parse_nmea(buff,4) ;
+}
+get_lon(*buff){
+  parse_nmea(buff,5) ;
+}
+get_lon_hem(*buff){
+  parse_nmea(buff,6)
+}
+parse_nmea(*buff,int count){
   char *p1, *p2 ;
   char temp[12];
   p1 = buff ;
   unsigned char i = 0 ;
-  printf("\n\rget_lat_str\n\r");
   for(i=0;i<count;i++){
   p2 = strstr(p1,",");
   p1 = p2+1 ;
-  printf("\r\np1 : %s\r\n",p1);
   }
-
   p2 = strstr(p1,",");
-  printf("\r\ntemp :%s \r\n",temp);
-
   memset(temp,0x0,10);
-  printf("\r\ntemp af : %s\r\n", temp);
-  memcpy(temp,p2,p2-p1);
-  printf("\r\nfinal temp buff : %s\r\n",temp);
+  memcpy(temp,p1,p2-p1);
+  printf("\r\n%s\r\n",temp);
 
  
 }
